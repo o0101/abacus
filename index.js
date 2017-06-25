5735aa43e91759e668ffadd7aeab23634cebb8f2
Cris <cris@dosaygocorporation>
2017-06-25 20:32:29 +0800

    Basically works

--- index.js
+++ index.js
@@ -18,41 +18,66 @@
     return Math.max( ...v.map( b => b.length ) );
   }
 
-  function pointwise( op, initial, does_carry, does_borrow, ...v ) {
-    console.log( v );
-    const maxlen = to_maxlen( ...v );
-    const vnum = v.length;
-    console.log( maxlen, vnum );
-    const x = new Uint1Array( maxlen + (does_carry ? 1 : 0 ) );
+  function to_carry( n, b ) {
+    const point = n % b; 
+    const carry = (n-point)/b;
+    return { point, carry };
+  }
+  
+  function to_borrow( n, b ) {
+    console.log("nb", n,b);
+    if ( n >= 0 ) {
+      return { point: n, borrow : 0 };
+    }
+    let { point, carry } = to_carry( n, b ); 
+    const borrow = carry + 1;
+    carry += 1;
+    point += borrow*b;
+    point %= b;
+    console.log("pb", point, borrow );
+    return { point, borrow };
+  }
 
-    let carry = initial;
+  function pointwise( op, initial, does_carry, does_borrow, ...v ) {
+    const maxlen = to_maxlen( ...v ) + ( does_carry ? 1 : 0 );
+    const head = v.shift();  
+    const vnum = v.length;
+
+    const x = new Uint1Array( maxlen );
+    x.set( head );
+
+    let carriage = initial;
+    let borrowance = initial;
     for( let i = 0; i < maxlen; i++ ) {
-      let xi = initial;
-      if ( does_carry ) {
-        xi = op( xi, carry );
-      }
+      let xi = x[i];
       for( let j = 0; j < vnum; j++ ) {
-        if ( i >= v[j].length ) continue;
-        if ( xi >= Number.MAX_SAFE_INTEGER ) {
-          throw new ValueError( `Precision Loss Failure: intermediate value xi has exceeded JavaScript's maximum precise value.` );
+        if ( i >= v[j].length ){
+          continue;
         }
+
         xi = op( xi, v[j][i] );
       }
-      x[i] = xi & 1;
       if ( does_carry ) {
-        if ( xi < 0 ) xi = Math.abs(xi);
-        carry = xi - x[i];
-        if ( ! does_borrow ) {
-          carry >>= 1;
-        }
+        xi = op(xi, carriage);
+        const { point, carry } = to_carry( xi, 2 ); 
+        x[i] = point;
+        carriage = carry;
+      }
+      if ( does_borrow ) {
+        console.log( borrowance );
+        xi = op(xi, borrowance);
+        const { point, borrow } = to_borrow( xi, 2 ); 
+        x[i] = point;
+        borrowance = borrow;
       }
     }
-    if ( does_carry && carry ) {
-      console.log("LAST CARRY", carry );
-      x[maxlen] = carry;
+    if ( does_carry && carriage ) {
+      x[maxlen] = carriage;
+    }
+    if ( does_borrow && borrowance ) {
+      console.log("WHAT HERE?", borrowance );
     }
 
-    console.log(x+"");
     return x;
   }
 
@@ -77,23 +102,33 @@
   }
 
   function dif( ...v ) {
-    return pointwise( (a,b) => a - b, 0, true, true, ...v );
+    const head = v.shift();
+    const tail = add( ...v );
+    return pointwise( (a,b) => a - b, 0, false, true, head, tail );
   }
 
   // implement a convolve and mul calls convolve
-  function mul( basis, scaler ) {
-    const product = new Uint1Array( basis.length + scaler.length - 1 ); 
+  function mul( ...v ) {
+    let product = v.shift();
+    while( v.length ) {
+      product = mul2arity( product, v.shift() );
+    }
+    return product;
+  }
+
+  function mul2arity( basis, scaler ) {
+    const product = new Uint1Array( basis.length + scaler.length ); 
     const scaled_bases = [];
     for( let i = 0; i < scaler.length; i++ ) {
       if ( scaler[i] ) {
-        scaled_bases.push( mul2( basis, i ) );
+        scaled_bases.push( mulbypowerof2( basis, i ) );
       }
     }
     product.set( add( ...scaled_bases ) );
     return product;
   }
 
-  function mul2( basis, power ) {
+  function mulbypowerof2( basis, power ) {
     const scaled_basis = new Uint1Array( basis.length  + power );
     scaled_basis.set( basis, power );
     return scaled_basis;
--- test.js
+++ test.js
@@ -3,13 +3,11 @@
   const Uint1Array = require('uint1array');
   const bitmath = require('./index.js');
 
-  const a = Uint1Array.of( 1, 1, 1, 1, 1 );
-  const b = Uint1Array.of( 1 );
+  const a = Uint1Array.of( 1, 1, 1, 1, 1, 1, 1 );
+  const b = Uint1Array.of( 1, 1, 0, 0, 0, 1);
   const c = bitmath.add(a,b);
-  const d = bitmath.dif(a,b);
-  const e = Uint1Array.of( 0, 1 );
-  const f = bitmath.dif(c,b);
-  console.log( `a=${a}\nb=${b}\nc=a+b=${c}\nd=a-b=${d}\ne=${e}\nf=c-e=${f}\n`);
-  const g = bitmath.inv( Uint1Array.of( 0,1,1,0,1,0,0,1 ) );
-  console.log( `inv(01101001) = ${g}` );
+  const f = bitmath.dif(a,b);
+  const p = bitmath.mul(a,b);
+  const q = bitmath.mul(a,a,b,b,b);
+  console.log( `a=${a}\nb=${b}\nc=a+b=${c}\nf=a-b=${f}\np=a*b=${p}\nq=a*a*b*b*b=${q}`);
 }
